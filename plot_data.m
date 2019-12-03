9cb1b47b82f9eddd33c96042e274df73410ce844
Xihan Ma <xma4@wpi.edu>
2019-12-03 12:32:25 -0500

    add plots for part2

--- plot_data.m
+++ plot_data.m
@@ -1,4 +1,4 @@
-# read joint values and plot the data
+% # read joint values and plot the data
 
 clc
 clear
@@ -12,7 +12,7 @@ joint_states = rossubscriber('/custom_scara/joint_states');
 
 joint3 = [];
 rate = robotics.Rate(20);
-record_time = 10;
+record_time = 8;
 reset(rate);
 
 while rate.TotalElapsedTime < record_time
@@ -24,11 +24,16 @@ end
 
 disp('complete');
 
+%%
 t = linspace(0,record_time,length(joint3));
+ref = 0.05*ones(1,length(joint3));
 plot(t,joint3,'b','LineWidth',1);
-grid on;
+hold on
+plot(t,ref,'-.m','LineWidth',1);
+grid on
 xlabel('time [sec]');
 ylabel('joint3 position [rad]');
+legend('actual joint pose','desired joint pose');
 
 
 
