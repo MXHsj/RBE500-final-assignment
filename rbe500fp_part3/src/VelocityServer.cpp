814ade570321ae97f3728307a2c680468ea7f85b
Ellie <edfoltan@wpi.edu>
2019-12-10 13:19:03 -0500

    Velocity Server just needs testing and topic names

--- rbe500fp_part3/src/VelocityServer.cpp
+++ rbe500fp_part3/src/VelocityServer.cpp
@@ -21,6 +21,37 @@ ros::Publisher command_pub_j1;
 ros::Publisher command_pub_j2;
 ros::Publisher command_pub_j3;
 
+class Gazebo_Listener{
+public:
+    float q1;
+    float q2;
+    float q3;
+    void update_jointOne(const control_msgs::JointControllerState::ConstPtr &msg);
+    void update_jointTwo(const control_msgs::JointControllerState::ConstPtr &msg);
+    void update_jointThree(const control_msgs::JointControllerState::ConstPtr &msg);
+};
+
+void Gazebo_Listener::update_jointOne(const control_msgs::JointControllerState::ConstPtr &msg)
+{
+    q1 = msg->set_point;
+    if (std::abs(q1) < tolerence)
+        q1 = 0;
+}
+void Gazebo_Listener::update_jointTwo(const control_msgs::JointControllerState::ConstPtr &msg)
+{
+    q2 = msg->set_point;
+    if (std::abs(q2) < tolerence)
+        q2 = 0;
+}
+void Gazebo_Listener::update_jointThree(const control_msgs::JointControllerState::ConstPtr &msg)
+{
+    q3 = msg->set_point;
+    if (std::abs(q3) < tolerence)
+        q3 = 0;
+}
+
+
+
 // Recieve a Reference Position 
 bool goTo(rbe500fp_part3::RefVel::Request &req, rbe500fp_part3::RefVel::Response &res)
 {
@@ -50,12 +81,22 @@ bool goTo(rbe500fp_part3::RefVel::Request &req, rbe500fp_part3::RefVel::Response
   RefSrv.request.day = 0;
   RefSrv.request.daz = 0;
 
-  /*************************************************
-    THIS NEEDS TO BE FILLED IN BY READING FROM TOPIC
-  **************************************************/
-  RefSrv.request.q1 = ?????;
-  RefSrv.request.q2 = ?????;
-  RefSrv.request.q3 = ?????;
+  Gazebo_Listener g_listener;
+
+
+  ros::Subscriber jointOne = n.subscribe("/custom_scara/joint1_position_controller/state", 1, &Gazebo_Listener::update_jointOne, &g_listener);
+  ros::Subscriber jointTwo = n.subscribe("/custom_scara/joint2_position_controller/state", 1, &Gazebo_Listener::update_jointTwo, &g_listener);
+  ros::Subscriber jointThree = n.subscribe("/custom_scara/joint3_position_controller/state", 1, &Gazebo_Listener::update_jointThree, &g_listener);
+  
+  for (int i = 0; i < 3; i ++)
+  {
+    ros::spinOnce();
+    loop_rate.sleep();
+  }
+
+  RefSrv.request.q1 = g_listener.q1;
+  RefSrv.request.q2 = g_listener.q2;
+  RefSrv.request.q3 = g_listener.q3;
 
 
   // Define messages to publish to controller
@@ -89,9 +130,9 @@ bool goTo(rbe500fp_part3::RefVel::Request &req, rbe500fp_part3::RefVel::Response
   }
  
   // Publish the reference position to the Gazebo topic
-  command_pub_j1.publish(messageJoint1);
-  command_pub_j2.publish(messageJoint2);
-  command_pub_j3.publish(messageJoint3);
+    command_pub_j1.publish(messageJoint1);
+    command_pub_j2.publish(messageJoint2);
+    command_pub_j3.publish(messageJoint3);
 
 	ROS_INFO("Request: Joint 1 Reference = %f, Joint 2 Reference = %f, Joint 3 Reference = %f", (float)req.ref1, (float)req.ref2, (float)req.ref3);
 	ROS_INFO("Sending Back Response: Complete");
@@ -105,19 +146,18 @@ int main(int argc, char **argv)
 {
   ros::init(argc, argv, "Velocity_Server");
   ros::NodeHandle n;
-
+  ros::Rate loop_rate(30);
+    
   // Create a service for recieving reference position
   ros::ServiceServer service = n.advertiseService("VelServer", goTo);
 
   // Create a publish relation to the SCARA command topic
-  
   /***********************************
     THIS NEEDS TO BE FILLED IN LATER
   ************************************/
   command_pub_j1 = n.advertise<std_msgs::Float64>("/FILL IN HERE", 1000);
   command_pub_j2 = n.advertise<std_msgs::Float64>("/FILL IN HERE", 1000);
   command_pub_j3 = n.advertise<std_msgs::Float64>("/FILL IN HERE", 1000);
-
 
   ROS_INFO("Ready to Move the SCARA Robot");
 
