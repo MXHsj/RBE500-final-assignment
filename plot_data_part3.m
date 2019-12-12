4fb923a2421fa522aa0927178009f5d2a0237824
Xihan Ma <xma4@wpi.edu>
2019-12-12 18:47:31 -0500

    changed matlab for part3

--- plot_data_part3.m
+++ plot_data_part3.m
@@ -12,22 +12,31 @@ joint_states = rossubscriber('/custom_scara/joint_states');
 joint1_rec = [];
 joint2_rec = [];
 joint3_rec = [];
-rate = robotics.Rate(20);
+rate = robotics.Rate(30);
 record_time = 10;
 reset(rate);
 
 joint_init = receive(joint_states);
-joint1_init = joint_init.Position(1);
-joint2_init = joint_init.Position(2);
-joint3_init = joint_init.Position(3);
+joint1_old = joint_init.Position(1);
+joint2_old = joint_init.Position(2);
+joint3_old = joint_init.Position(3);
+t_old = 0;
 
 while rate.TotalElapsedTime < record_time
     t = rate.TotalElapsedTime;
+    dt = t - t_old;
     joint_update = receive(joint_states);
-    joint1_rec = [joint3_rec, (joint_update.Position(3) - joint3_init)/t];
-    joint2_rec = [joint3_rec, (joint_update.Position(3) - joint3_init)/t];
-    joint3_rec = [joint3_rec, (joint_update.Position(3) - joint3_init)/t];
+    joint1_curr = joint_update.Position(1);
+    joint2_curr = joint_update.Position(2);
+    joint3_curr = joint_update.Position(3);
+    joint1_rec = [joint1_rec, (joint1_curr - joint1_old)/dt];
+    joint2_rec = [joint2_rec, (joint2_curr - joint2_old)/dt];
+    joint3_rec = [joint3_rec, (joint3_curr - joint3_old)/dt];
     disp('recording ...');
+    joint1_old = joint1_curr;
+    joint2_old = joint2_curr;
+    joint3_old = joint3_curr;
+    t_old = t;
     waitfor(rate);
 end
 
